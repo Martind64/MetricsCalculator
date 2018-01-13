71d660a46a012401c6329bd43bfb99d52d4b6f44
Martin Delfs <made@html24.net>
2018-01-13 12:54:30 +0100

    invaluable logic

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+import javax.swing.*;
+
 public class Controller {
+
+    @FXML
+    private Text actiontarget;
+    @FXML
+    private Text secondActiontarget;
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Casper is a fat fuck");
+    }
+
+    @FXML protected void removeText(ActionEvent event){
+        secondActiontarget.setText("HAHA no way, you can't obscure the truth!");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("converter.fxml"));
+        primaryStage.setTitle("Metric Converter");
         primaryStage.setScene(new Scene(root, 1300, 825));
+
         primaryStage.show();
     }
 
